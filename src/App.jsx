f2669794dc35d7ab7541f7d81dfbdfafecb7ce8e
fofalafofolle <ncirfarah7@gmail.com>
2025-10-16 13:59:55 +0100

    exercice 3 - second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,37 @@
+/* exercice 1 */
+/*import Bienvenue from './Bienvenue';
+
+function App() {
+  return (
+    <div>
+      <Bienvenue nom="Farah" />
+    </div>
+  );
+}
+
+export default App;*/
+
+
+
+/*exercice 2 */ 
+/*import './App.css'
+import Compteur from './Compteur'
+
+function App() {
+  return (
+    <div>
+      <h1>Mon app React</h1>
+      <Compteur />
+    </div>
+  )
+}
+
+export default App*/
+
+
+
+
+/*exercice 3*/
 import "./App.css";
 import Formulaire from "./Formulaire";
 
