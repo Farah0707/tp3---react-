819009f6a9b6a4882683a6622e6d6431027fe87c
fofalafofolle <ncirfarah7@gmail.com>
2025-10-16 13:33:13 +0100

    exercice 1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Bienvenue from "./Bienvenue";
+
+function App() {
+  return (
+    <div>
+      <h1>Mon app React</h1>
+      <Bienvenue name="Farah" />
+      <Bienvenue name="Siwar" />
+    </div>
+  );
+}
+
+export default App;
